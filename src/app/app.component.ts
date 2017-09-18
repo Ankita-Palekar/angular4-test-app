d906fd78558a7b4369b887b8ce7dbbbbc57a830b
ankita <ankita@cdmx.in>
2017-09-18 18:59:05 +0530

    adding up services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,6 @@
 import { Component } from '@angular/core';
 import {Hero} from './hero';
-
-const HEROES: Hero[] = [
-  { id: 1, name: 'Mr. Nice' },
-  { id: 2, name: 'Narco' },
-  { id: 3, name: 'Bombasto' },
-  { id: 4, name: 'Celeritas' },
-  { id: 5, name: 'Magneta' },
-  { id: 6, name: 'RubberMan' },
-  { id: 7, name: 'Dynama' },
-  { id: 8, name: 'Dr IQ' },
-  { id: 9, name: 'Magma' },
-  { id: 10, name: 'Tornado' }
-]
+import {HeroService} from './hero.service';
 
 @Component({
   selector: 'app-root',
@@ -21,8 +9,9 @@ const HEROES: Hero[] = [
 })
 
 export class AppComponent {
+  constructor(private heroService: HeroService) { }
   title = 'Heroes';
-  heroes = HEROES;
+  heroes = Hero;
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
